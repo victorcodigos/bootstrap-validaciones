376273a69d9cd8a1f6c35006f652b03d4cf6d256
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-06-08 17:20:39 +0200

    modificando JS, validacion hecha!

--- main.js
+++ main.js
@@ -8,9 +8,6 @@ const password = document.querySelector("#password");
 const email = document.querySelector("#email");
 const parrafo = document.querySelector(".parrafo")
 
-
-
-
 function onSubmit(e) {
   e.preventDefault();
   const nombre = inputNombre.value;
@@ -26,19 +23,18 @@ function onSubmit(e) {
     correo: emailValue,
     passwordUno: inputPassUnoValue,
     passwordDos: inputPassDosValue,
-
   };
 
   verify()
-
   localStorage.setItem("usuario", JSON.stringify(user));
   console.log(user)
 
 }
 // funcion para borrar todo el local storage con el boton borrar todos local
 
-function clearLocal() { localStorage.clear() };
-
+function clearLocal() {
+  localStorage.clear()
+};
 
 inputBtn.addEventListener("click", onSubmit);
 
@@ -47,31 +43,31 @@ inputBtn.addEventListener("click", onSubmit);
 let datos = {};
 
 function verify() {
-  const password = " ^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,8}$";
+  const password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,8}$/;
   //requires one lower case letter, one upper case letter,  one digit, 6-13 length, and no space.
   //Matches	== 1agdA*$# | 1agdA*$# | 1agdA*$#
 
 
-  const email = "^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+  const email = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
   // requires Email validator that adheres directly to the specification for email address naming
   // Matches	asmith@mactec.com | foo12@foo.edu | bob.smith@foo.tv
 
 
   if (inputNombre.value === '' || inputEmail.value === '' || inputPassUno.value === '' || inputPassDos.value === '') {
-
-  console.log("Inserir todos los dados");
-  
-
-  msg.innerHTML = `<p class= "p-error" >"Please enter all fields"</p> `;
-
-
-} 
-
-
-else (password.valueOf) (email.valueOf) === (true);
-  
-  {
-  msg.innerHTML = ("Please enter a correct email")
- }
-  
+    console.log("error campos vacios")
+    msg.innerHTML = `<p class= "p-error" >"Please enter all fields"</p> `;
+  }
+  else if (email.test(inputEmail.value) !== true) {
+    msg.innerHTML = `<p class= "p-error" >Please enter a valid email</p> `;
+  }
+  else if (password.test(inputPassUno.value) !== true) {
+    msg.innerHTML = `<p class= "p-error">"Please enter a valid password"</p>`;
+  }
+  else if (password.test(inputPassUno.value !== inputPassDos.value) !== true) {
+    msg.innerHTML = `<p class= "p-error">"It must be the same password"</p>`;
+  }
+  else {
+    msg.innerHTML = `<p class= "p-success">"Usuario creado correctamente"</p>`;
+  }
+  setTimeout(() => { msg.innerHTML = ""; }, 5000);
 }
